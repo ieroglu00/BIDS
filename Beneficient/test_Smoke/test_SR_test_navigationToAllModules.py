8f7d7331cfe6aa3caf5da88e479cf2e18f97a3af
Neeraj <neerajpebma0@gmail.com>
2021-09-03 15:23:39 +0530

    Email sending code and PDF report code

---
+++ Beneficient/test_Smoke/test_SR_test_navigationToAllModules.py
@@ -0,0 +1,42 @@
+import os
+import smtplib
+from email.message import EmailMessage
+
+import openpyxl
+import pytest
+
+
+@pytest.mark.smoke
+def test_ReportSend_AllModulesVerify():
+    print()
+    TestName = "neeraj"
+    loc = ('C:/Users/Neeraj/PycharmProjects/beneficienttest/Beneficient/' + TestName + '.xlsx')
+    wb=openpyxl.load_workbook(loc)
+    sheet = wb.active
+    PDFName=sheet.cell(1, 1).value
+    print(PDFName)
+
+    msg=EmailMessage()
+    # mention failure and pass status ---------------------------
+    msg['Subject']='Automation Test Report'
+    msg['From']='Neeraj'
+    msg['To']='neeraj.kumar@crochetech.com'
+    #msg.set_content("Test email from Neeraj")
+
+    with open('C:/Users/Neeraj/PycharmProjects/beneficienttest/Beneficient/test_Smoke/EmTemp.txt') as myfile:
+        data=myfile.read()
+
+    with open('C:/Users/Neeraj/PycharmProjects/beneficienttest/Beneficient/test_Smoke/'+PDFName, 'rb') as f:
+        file_data = f.read()
+        file_name = f.name
+
+    msg.set_content(data)
+    msg.add_attachment(file_data, maintype='application', subtype='octet-stream', filename=file_name)
+
+    server=smtplib.SMTP_SSL('smtp.gmail.com',465)
+    server.login("neeraj.kumar@bitsinglass.com","Motorola@408")
+
+    server.send_message(msg)
+    print("Test Report sent")
+    os.remove('C:/Users/Neeraj/PycharmProjects/beneficienttest/Beneficient/test_Smoke/'+PDFName)
+    server.quit()
