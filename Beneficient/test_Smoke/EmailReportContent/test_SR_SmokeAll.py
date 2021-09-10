b9513ac12a27840bd5642b03ceb682de1882231d
Neeraj <neerajpebma0@gmail.com>
2021-09-10 13:41:21 +0530

    General Push

--- Beneficient/test_Smoke/EmailReportContent/test_SR_SmokeAll.py
+++ Beneficient/test_Smoke/EmailReportContent/test_SR_SmokeAll.py
@@ -25,7 +25,7 @@ def test_ReportSendSmokeAll():
     msg=EmailMessage()
     msg['Subject']='Test SUITE Automation Report'
     msg['From']='Neeraj'
-    msg['To']='neeraj.kumar@crochetech.com'
+    msg['To']='neeraj.kumar@crochetech.com,a'
 
     with open('C:/BIDS/beneficienttest/Beneficient/test_Smoke/EmailReportContent/EmTemp.txt') as myfile:
         data=myfile.read()
