8260ea97c58c86a2259c56fa4c729c9c85c2e209
neerajk <neerajpebma0@gmail.com>
2021-09-17 07:11:39 +0000

    General Push

--- Beneficient/test_Smoke/test_Funds/test_VerifyingAllLinksFunds.py
+++ Beneficient/test_Smoke/test_Funds/test_VerifyingAllLinksFunds.py
@@ -57,7 +57,7 @@ def test_setup():
       enter_username("neeraj.kumar")
       enter_password("Crochet@786")
       driver.find_element_by_xpath("//input[@type='submit']").click()
-      print()
+
 
   yield
   if Exe == "Yes":
