d3d330f07321d1a5a1df8b69780060e074f0b5e2
Neeraj <neerajpebma0@gmail.com>
2021-10-25 16:35:25 +0530

    General push

--- Beneficient/test_Values_Check/EmailReportContent/SR_SmokeAll_together.py
+++ Beneficient/test_Values_Check/EmailReportContent/SR_SmokeAll_together.py
@@ -74,7 +74,7 @@ def test_ReportSendValuesCheck():
         msg.attach(file_attachment)
 
     email_from = 'Test Automation Team'
-    email_to = ['neeraj.kumar@crochetech.com']
+    email_to =['erin.Twiss@beneficient.com', 'neeraj.kumar@crochetech.com', 'srinath.jandhyala@beneficient.com', 'olufunke.okeni@beneficient.com','gregory.bybee@beneficient.com']
     SenderEmail="neeraj.kumar@bitsinglass.com"
     SenderPassword="Motorola@408"
 
