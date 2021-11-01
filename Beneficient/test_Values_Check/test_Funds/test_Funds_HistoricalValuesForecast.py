a4e0f3e6b6134ad8e981ffd19fb4377ddb5a062c
Neeraj <neerajpebma0@gmail.com>
2021-11-01 20:41:28 +0530

    General push

--- Beneficient/test_Values_Check/test_Funds/test_Funds_HistoricalValuesForecast.py
+++ Beneficient/test_Values_Check/test_Funds/test_Funds_HistoricalValuesForecast.py
@@ -534,7 +534,7 @@ def test_Funds_Values(test_setup):
                         except Exception:
                             break
                     try:
-                        time.sleep(4)
+                        time.sleep(5)
                         driver.find_element_by_xpath(
                             "//div[@class='ContentLayout---content_layout']/div/div/div/div[4]/div/div/div/div/div/div[2]/div/div/div[3]/div[2]/div/div[1]/div[2]/table/tbody/tr/td[2]/div/p/a[contains(text(),'" +
                             FundsNamesList[ii3] + "')]").click()
@@ -549,7 +549,7 @@ def test_Funds_Values(test_setup):
                                 time.sleep(1)
                             except Exception:
                                 break
-                        time.sleep(3)
+                        time.sleep(5)
                         try:
                             buttonFundName = driver.find_element_by_xpath(
                                 "//div[@class='ContentLayout---content_layout']/div/div/div/div[4]/div/div/div/div/div/div[2]/div/div/div[3]/div[2]/div/div[1]/div[2]/table/tbody/tr/td[2]/div/p/a[contains(text(),'" +
