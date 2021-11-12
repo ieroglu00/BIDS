467a9eb2f7994c9f01b0795f9f289eaaba9456aa
Neeraj <neerajpebma0@gmail.com>
2021-11-12 20:12:51 +0530

    General push

--- Beneficient/test_Cont_Dist/EmailReportContent/SR_SmokeAll_together.py
+++ Beneficient/test_Cont_Dist/EmailReportContent/SR_SmokeAll_together.py
@@ -80,7 +80,7 @@ def test_ReportSendSmokeAll():
 
     date_str = pd.Timestamp.today().strftime('%m-%d-%Y')
     msg = MIMEMultipart()
-    msg['Subject']='Test Automation Report [Smoke Test 5] -Env [Test] '+date_str
+    msg['Subject']='Test Automation Report [Smoke Test 5 ( Cont & Dist )] -Env [Test] '+date_str
     msg['From'] = email_from
     msg['To'] = ','.join(email_to)
     msg.attach(MIMEText(html, "html"))
